e0b5a3cf64cc0fd8a1816c71a500208d92f6ea40
Benjamin Lenhardt <benilen.350@gmail.com>
2021-12-25 14:53:59 +0100

    Recosions aufruf intlesen

--- lib/hello_world.dart
+++ lib/hello_world.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 import 'dart:io';
+import 'package:hello_world/utils.dart';
+
 import 'autos.dart';
 
 void myLittleMain() {
@@ -15,26 +17,23 @@ void myLittleMain() {
   int aufarbeit = 7;
   
   
- // Next time: funktion zum commandozeilen parsen
+ // Next time: funktion zum commandozeilen parsen +github
   while(true)
   {    
     print("Was soll gemacht werden? \n 0: fahren \n 1: dach öffnen \n 2: dach schließen");
-    var line = stdin.readLineSync(encoding: utf8);
-    if (line != null ){
-      int entscheidung = int.parse(line.trim());
-      switch (entscheidung) {
-        case 0: autofahren(flitzer); break;
-        case 1: flitzer.dachOeffnen(); break;
-        case 2: flitzer.dachschliessen(); break;
-        default: print("Ungülitge eingabe");
-      }
+    int? entscheidung = integerLesen();
+    switch (entscheidung) {
+      case 0: autofahren(flitzer); break;
+      case 1: flitzer.dachOeffnen(); break;
+      case 2: flitzer.dachschliessen(); break;
+      default: print("Ungülitge eingabe");
+    }
 
-     }
   }
-  
-  
 
 }
+
+
 
 void autofahren(Auto auto){
   while(true){
